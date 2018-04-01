ce924c23e89b2bfe1201cb85739b972e8a70ca9c
Phil Lloyd <philwlloyd@localhost.susx.ac.uk>
2018-04-01 12:44:52 +0100

    Dice and GameType interface

---
+++ src/main/java/Dice.java
@@ -0,0 +1,20 @@
+/**
+ * The dice class is for when dice are rolled and counting doubles
+ */
+public class Dice {
+
+    /**
+     * doubleCount: Int
+     * This integer value counts the amount of doubles a player rolls in a row.
+     */
+    private int doubleCount;
+
+    /**
+     * roll
+     * @return An array of integers
+     * This method returns the two values between 1 and 6 shown on the two dice. The function will also add to doubleCount where appropriate.
+     */
+    public int[] roll(){
+        return null;
+    }
+}
--- src/main/java/GameType.java
+++ src/main/java/GameType.java
@@ -1,4 +1,16 @@
+/**
+ * This enumeration will show different game type options, this could be added to.
+ */
 public enum GameType {
-    FULL,
-    Abridged
+    /**
+     * FullGame
+     * In the full game there is no time limit. A winner is determined by whoever is the last player with funds.
+     */
+    FullGame,
+
+    /**
+     * AbridgedGame
+     * In the abridged game there is a time limit defined in the UI. At the end of the time the winner is the player with the most assets.
+     */
+    AbridgedGame
 }
