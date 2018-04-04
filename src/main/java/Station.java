3774e15d9e9b6958225c3e17639163dacc4418ac
Guy Mac <hello@guymac.eu>
2018-04-04 03:26:02 +0100

    Add facility tiles

---
+++ src/main/java/Station.java
@@ -0,0 +1,10 @@
+public class Station extends Ownable implements Facility {
+
+    public Station(String name, int position){
+        super(name, position);
+    }
+
+    public void calculateRent() {
+        // TODO
+    }
+}
---
+++ src/main/java/Utility.java
@@ -0,0 +1,10 @@
+public class Utility extends Ownable implements Facility {
+
+    public Utility(String name, int position){
+        super(name, position);
+    }
+
+    public void calculateRent() {
+        // TODO
+    }
+}
