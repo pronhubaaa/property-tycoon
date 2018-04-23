07d74c71809940346a43e41f9f2785edce8b1736
Guy Mac <hello@guymac.eu>
2018-04-23 17:19:46 +0100

    Revert CardType.java
    This commit reverts the changes to CardType.java in 72640880a8078e05c737491520939ade6fe255ec

--- src/main/java/CardType.java
+++ src/main/java/CardType.java
@@ -1,3 +1,19 @@
 public enum CardType {
-    POT_LUCK, OPPORTUNITY_KNOCKS;
+    PlotLuck("Plot Luck"),
+    OpportunityKnocks("Opportunity Knocks");
+
+    private String value;
+
+    CardType(final String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return this.getValue();
+    }
 }
