9735492c3dc76410622dc75e0757a956500e89ac
Guy Mac <hello@guymac.eu>
2018-04-25 09:29:49 +0100

    Listify cards.json actions

--- src/main/java/resources/parseCardJson.php
+++ src/main/java/resources/parseCardJson.php
@@ -4,11 +4,20 @@
 
 	foreach ($json as $i => $card) {
 		foreach ($card as $key => $value) {
-			if (empty($value)) {
+			if (empty($value) || $key == 'action_desc') {
+				unset($json[$i][$key]);
+			}
+			if (!in_array($key, array('card_type', 'action_desc'))) {
+				if (in_array($key, array('desc', 'action_type'))) {
+					$json[$i]['actions'][0][$key] = $value;
+				} else {
+					$json[$i]['actions'][0]['params'][$key] = $value;
+				}
 				unset($json[$i][$key]);
 			}
 		}
 	}
 	$json = json_encode($json, JSON_PRETTY_PRINT);
+	echo $json;
 	file_put_contents('cards.json', $json);
 ?>
