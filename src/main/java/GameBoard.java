b8ad81c739d5cfd705f6a36c0a8251a947b393c8
Elliot Massen <hello@elliotmassen.com>
2018-04-25 13:40:07 +0100

    Beginning to set up tils.

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -1,14 +1,91 @@
-import javafx.scene.layout.Pane;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+
+import java.util.ArrayList;
 
 public class GameBoard {
     private GameEngine _gameEngine;
     private Tile _tiles[];
 
-    public GameBoard(GameEngine gameEngine) {
+    private StackPane _parentPane;
+    private HBox _sidebarSplitPane;
+    private BorderPane _boardPane;
 
+    private Button _detailsButton;
+
+    public GameBoard(GameEngine gameEngine) {
+        this._gameEngine = gameEngine;
     }
 
     public Pane getLayout() {
+        this._parentPane = new StackPane();
+        this._sidebarSplitPane = new HBox();
+        this._boardPane = new BorderPane();
 
+        this._parentPane.setMinWidth(400);
+        this._parentPane.setMinHeight(400);
+
+        this._createBoardLayout();
+
+        this._sidebarSplitPane.getChildren().add(this._boardPane);
+        this._parentPane.getChildren().add(this._sidebarSplitPane);
+
+        return this._parentPane;
+    }
+
+    private void _createBoardLayout() {
+        this._detailsButton = new Button();
+        this._detailsButton.setText("Property details");
+        this._detailsButton.setId("property-details");
+
+        ArrayList<Tile> tiles = this._gameEngine.getBoard().getTiles();
+        ArrayList<Button> tileButtons = new ArrayList<Button>();
+
+        for(Tile tile : tiles) {
+            Button tileButton = new Button();
+            tileButton.setText(tile.getName());
+            tileButton.setId("tile-" + Integer.toString(tile.getPosition()));
+            tileButton.setMinWidth(40);
+            tileButton.setMinHeight(40);
+
+            if(tile instanceof Ownable) {
+                tileButton.setOnAction((ActionEvent e) -> {
+                    if(this._detailsButton.getParent() == null) {
+                        this._parentPane.getChildren().add(this._detailsButton);
+                    }
+                    else {
+                        this._parentPane.getChildren().remove(this._detailsButton);
+                    }
+                });
+            }
+
+            tileButtons.add(tileButton);
+        }
+
+        HBox bottomRow = new HBox();
+        HBox topRow = new HBox();
+        VBox leftColumn = new VBox();
+        VBox rightColumn = new VBox();
+
+        for(int i = 0; i < tileButtons.size(); i++) {
+            if(i < 10) {
+                bottomRow.getChildren().add(tileButtons.get(i));
+            }
+            else if(i < 20) {
+                leftColumn.getChildren().add(tileButtons.get(i));
+            }
+            else if(i < 30) {
+                topRow.getChildren().add(tileButtons.get(i));
+            }
+            else {
+                rightColumn.getChildren().add(tileButtons.get(i));
+            }
+        }
+
+        this._boardPane.setBottom(bottomRow);
+        this._boardPane.setTop(topRow);
+        this._boardPane.setLeft(leftColumn);
+        this._boardPane.setRight(rightColumn);
     }
 }
--- src/test/java/GameBoardTest.java
+++ src/test/java/GameBoardTest.java
@@ -1,36 +1,104 @@
+import com.alibaba.fastjson.JSONObject;
+import com.sun.javafx.robot.FXRobotFactory;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.hamcrest.CoreMatchers;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.testfx.api.FxAssert;
+import org.testfx.api.FxRobot;
 import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.matcher.base.NodeMatchers;
+
+import javax.sound.midi.SysexMessage;
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 public class GameBoardTest extends ApplicationTest {
-    private Scene _mainMenuScene;
-    private Stage _primaryStage;
-
     @BeforeClass
     public static void beforeClass() {
-        System.setProperty("testfx.robot", "glass");
-        System.setProperty("testfx.headless", "true");
-        System.setProperty("prism.order", "sw");
-        System.setProperty("prism.text", "t2k");
     }
 
     @Override
     public void start(Stage stage) {
+        URL boardUrl = getClass().getResource("./resources/board.json");
+        File boardFile = new File(boardUrl.getPath());
+
+        String boardJSON = null;
+        try {
+            boardJSON = new Scanner(boardFile).useDelimiter("\\Z").next();
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        JSONObject boardData = (JSONObject) JSONObject.parse(boardJSON);
+
+        ArrayList<Player> players = new ArrayList<Player>(){{
+            add(new Player(10, "Elliot", null));
+            add(new Player(10, "Pete", null));
+            add(new Player(10, "Sam", null));
+        }};
+
         GameEngine gameEngine = null;
+        try {
+            gameEngine = new GameEngine(boardData, players, GameType.FullGame);
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+
         UI ui = new UI(stage, gameEngine);
         stage.setTitle("Property Tycoon");
 
-        this._mainMenuScene = MainMenuScreens.getMainMenu(ui);
-        ui.showScene(this._mainMenuScene);
-        stage.show();
+        GameBoard gameBoard = new GameBoard(gameEngine);
 
-        this._primaryStage = stage;
+        ui.showScene(this.createSandboxScene(gameBoard));
+        //stage.setFullScreen(true);
+        stage.show();
+    }
+
+    public Scene createSandboxScene(GameBoard gameBoard) {
+        return new Scene(gameBoard.getLayout());
     }
 
     @Test
-    public void getMainMenu() {
+    public void canDisplayAndHidePropertyDetails() {
+        // Property details should not be visible initially
+        FxAssert.verifyThat("#property-details", NodeMatchers.isNull());
 
+        // Assert that a tile exists
+        Node tileButton = lookup("#tile-2").query();
+        assertThat(tileButton, is(not(nullValue())));
+        FxAssert.verifyThat("#tile-2", NodeMatchers.isVisible());
+
+        // Click the tile
+        clickOn("#tile-2");
+
+        // We expect that property details will appear
+        Node detailsNode = lookup("#property-details").query();
+        assertThat(detailsNode, is(not(nullValue())));
+        FxAssert.verifyThat("#property-details", NodeMatchers.isVisible());
+
+        // Click the tile again
+        clickOn("#tile-2");
+
+        try {
+            wait(20000);
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        // When we click again it should disappear
+        detailsNode = lookup("#property-details").query();
+        assertThat(detailsNode, is(nullValue()));
+        FxAssert.verifyThat("#property-details", NodeMatchers.isNull());
     }
 }
