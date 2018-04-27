723333abf39ce7d2d6fcd283a21c259ade5ed88d
Peter Lloyd <pete_l@hotmail.co.uk>
2018-04-27 19:47:52 +0100

    React documentation website

---
+++ website/siteConfig.js
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// See https://docusaurus.io/docs/site-config.html for all the possible
+// site configuration options.
+
+/* List of projects/orgs using your project for the users page */
+const users = [
+  {
+    caption: 'User1',
+    image: 'img/main-logo.svg',
+    infoLink: 'https://www.facebook.com',
+    pinned: true,
+  },
+];
+
+const siteConfig = {
+  title: 'Property Tycoon' /* title for your website */,
+  tagline: 'Watson Games',
+  url: 'https://your-docusaurus-test-site.com' /* your website url */,
+  baseUrl: '/' /* base url for your project */,
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
+
+  // Used for publishing and more
+  projectName: 'property-tycoon',
+  organizationName: 'team-100-emoji',
+  // For top-level user or org sites, the organization is still the same.
+  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+  //   organizationName: 'JoelMarcey'
+
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [
+    //{page: 'help', label: 'Download'},
+    {doc: 'doc1', label: 'Documentation'},
+
+  ],
+
+  // If you have users set above, you add it here:
+  users,
+
+  /* path to images for header/footer */
+  headerIcon: 'img/icon.png',
+  footerIcon: 'img/main-logo.png',
+  favicon: 'img/icon.png',
+
+  /* colors for website */
+  colors: {
+    /*a2dcab*/
+    primaryColor: '#a2dcab',
+    secondaryColor: '#a2dcab',
+  },
+
+  /* custom fonts for website */
+  fonts: {
+    myFont: [
+      "Raleway",
+    ],
+    myOtherFont: [
+      "Raleway"
+    ]
+  },
+
+  // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
+  copyright:
+    'Copyright © ' +
+    new Date().getFullYear() +
+    ' Team 💯',
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks
+    theme: 'default',
+  },
+
+  // Add custom scripts here that would be placed in <script> tags
+  scripts: ['https://buttons.github.io/buttons.js'],
+
+  /* On page navigation for the current documentation page */
+  onPageNav: 'separate',
+
+  /* Open Graph and Twitter card images */
+  ogImage: 'img/main-logo.png',
+  twitterImage: 'img/main-logo.png',
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  //   repoUrl: 'https://github.com/facebook/test-site',
+};
+
+module.exports = siteConfig;
