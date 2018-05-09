ef0fe24a5f802da088e999c18b38931784c3fff7
Guy Mac <hello@guymac.eu>
2018-05-09 03:02:08 +0200

    Give test players names

--- src/test/java/GameBoardTest.java
+++ src/test/java/GameBoardTest.java
@@ -1,26 +1,21 @@
 import com.alibaba.fastjson.JSONObject;
-import com.sun.javafx.robot.FXRobotFactory;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import org.hamcrest.CoreMatchers;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.testfx.api.FxAssert;
-import org.testfx.api.FxRobot;
 import org.testfx.framework.junit.ApplicationTest;
 import org.testfx.matcher.base.NodeMatchers;
 
-import javax.sound.midi.SysexMessage;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.regex.Matcher;
-import static org.junit.Assert.*;
+
 import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
 
 public class GameBoardTest extends ApplicationTest {
     @BeforeClass
@@ -39,24 +34,28 @@ public class GameBoardTest extends ApplicationTest {
         String boardJSON = null;
         try {
             boardJSON = new Scanner(boardFile).useDelimiter("\\Z").next();
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
         JSONObject boardData = (JSONObject) JSONObject.parse(boardJSON);
 
-        ArrayList<Player> players = new ArrayList<Player>(){{
-            add(new Player(10, "Elliot", null));
-            add(new Player(10, "Pete", null));
-            add(new Player(10, "Sam", null));
+        ArrayList<Player> players = new ArrayList<Player>() {{
+            add(new Player(10, "Elliot", null) {{
+                setPiece(PlayerPiece.Cat);
+            }});
+            add(new Player(10, "Pete", null) {{
+                setPiece(PlayerPiece.Spoon);
+            }});
+            add(new Player(10, "Sam", null) {{
+                setPiece(PlayerPiece.HatStand);
+            }});
         }};
 
         GameEngine gameEngine = null;
         try {
             gameEngine = new GameEngine(boardData, players, GameType.FullGame);
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
@@ -97,8 +96,7 @@ public class GameBoardTest extends ApplicationTest {
 
         try {
             wait(20000);
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
@@ -113,7 +111,7 @@ public class GameBoardTest extends ApplicationTest {
         Set<Node> playerPanelQuery = lookup(".player-panel").queryAll();
         assertThat(playerPanelQuery.size(), is(3));
 
-        for(Node playerPanel: playerPanelQuery) {
+        for (Node playerPanel : playerPanelQuery) {
             assertThat(playerPanel, is(NodeMatchers.isVisible()));
         }
     }
